70830c1506694ded813f4f48a43f9b183ce79c9e
Dipayan Majumder <110282003+dipayan22@users.noreply.github.com>
2023-09-29 20:34:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,render_template
+import pickle
+import helper
+
+model=pickle.load(open('model.pkl','rb'))
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def find():
+    if request.method=='POST':
+        q1=request.form['ques1']
+        q2=request.form['ques2']
+
+        query=helper.query_point_creator(q1,q2)
+
+        result=model.predict(query)[0]
+
+        if result:
+            return render_template('index.html',prediction_text="This question's are Similar")
+
+        else:
+            return render_template('index.html',prediction_text="This question's are not Similar")
+
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
